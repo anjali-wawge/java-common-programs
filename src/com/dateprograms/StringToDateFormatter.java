5b3f5d0ee310cb298153d5ac73d087ad0a8cef07
anjali-wawge <anjalipwawge@gmail.com>
2022-07-29 22:17:51 +0530

    datePrograms and immutable class ex

--- src/com/dateprograms/StringToDateFormatter.java
+++ src/com/dateprograms/StringToDateFormatter.java
@@ -1,7 +1,9 @@
 package com.dateprograms;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class StringToDateFormatter {
 	public static void main(String[] args) {
@@ -21,5 +23,20 @@ public class StringToDateFormatter {
 		LocalDate localDate3=LocalDate.parse(date2, formatter3);
 		System.out.println(localDate3);
 		System.out.println(formatter3.format(localDate3));
+		
+		DateTimeFormatter formatter4=DateTimeFormatter.ofPattern("E,MMM dd yyyy");
+		String date3="Tue,Aug 16 2022";
+		LocalDate localDate4=LocalDate.parse(date3, formatter4);
+		System.out.println(formatter4.format(localDate4));
+		
+		DateTimeFormatter formatter5=DateTimeFormatter.ofPattern("EEEE,MMM dd yyyy");
+		String date4="Tuesday,Aug 16 2022";
+		LocalDate localDate5=LocalDate.parse(date4, formatter5);
+		System.out.println(formatter5.format(localDate5));
+		
+		DateTimeFormatter formatter6=DateTimeFormatter.ofPattern("EEEE,yyyy MMM, dd hh:mm:ss a",Locale.US);
+		String date5="Tuesday,2022 Aug, 16 11:10:55 PM";
+		LocalDateTime localDate6=LocalDateTime.parse(date5, formatter6);
+		System.out.println(formatter6.format(localDate6));
 	}
 }
--- src/com/java8practice/ListObjToArray.java
+++ src/com/java8practice/ListObjToArray.java
@@ -2,18 +2,23 @@ package com.java8practice;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class ListObjToArray {
 public static void main(String[] args) {
 	List<Integer> list=new ArrayList<>();
-	list.add(1);
-	list.add(2);
+	list.add(21);
+	list.add(12);
 	list.add(3);
 	list.add(4);
 	
-	Integer[] arr=list.stream().toArray(Integer[]::new);
-	for(Integer a:arr) {
-		System.out.println(a);
-	}
+	Integer[] arr=list.stream().toArray(Integer[]::new);//constructor reference
+//	System.out.println(Stream.of(arr).findFirst());
+	
+	//Stream.of(arr).forEach(System.out::println);//to print array
+	Stream.of(arr).sorted().forEach(System.out::println);;
+	/*
+	 * for(Integer a:arr) { System.out.println(a); }
+	 */
 }
 }
